4f43d85b1f9eee0728393d4e799501d780318191
Panagiotis <panfanartzis@gmail.com>
2022-09-01 16:11:13 +0300

    ongoose and models

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
 
-// listening on port 3000
-app.listen(3000);
+// connect to mongodb and then listening on port 3000
+const dbURI =
+  "mongodb+srv://panosfan:test1234@cluster0.splzwzk.mongodb.net/blogExpressApp?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    console.log("connected to db");
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
 
-// register view engine (ejs)
+// register view engine
 app.set("view engine", "ejs"); //default folder: views
 // app.set('views', 'myViews') an thelo na einai allo to folder ton views
 
@@ -33,6 +43,30 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "about" }); //gia xrisi me ejs (about.ejs mesa sto views folder) || this 2i param, pernao value sto view
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
+
+app.get("/blog/create", (req, res) => {
+  // const blog = new Blog({
+  //   title: "new blog",
+  //   snippet: "snippet",
+  //   body: "body of my new blog",
+  // });
+  // blog
+  //   .save()
+  //   .then((result) => res.send(result))
+  //   .catch((err) => console.log(err));
+  // opos akribos laravel models, mporo eite new Blog, eite na to kano amesos
+  Blog.create({
+    title: "new test",
+    snippet: "snip",
+    body: "body",
+  }).then((result) => res.send(result));
 });
 
 // redirect
