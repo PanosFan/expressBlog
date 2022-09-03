dc80932a906fa9778e40de2700e20401a63bff90
Panagiotis <panfanartzis@gmail.com>
2022-09-03 13:51:38 +0300

    moved script into public folder

---
+++ public/test.js
@@ -0,0 +1,32 @@
+console.log("batchest");
+const deleteBtn = document.querySelector("a.delete");
+deleteBtn.onclick = () => {
+  const endPoint = `/blogs/${deleteBtn.dataset.doc}`;
+  fetch(endPoint, { method: "DELETE" })
+    .then((response) =>
+      response.json().then((data) => (window.location.href = data.redirect))
+    )
+    .catch((err) => console.log(err));
+};
+const form = document.querySelector("#updateForm");
+const updateBtn = document.querySelector("a.update");
+const submitUpdateBtn = document.querySelector("#submitUpdateBtn");
+
+updateBtn.onclick = () => {
+  form.style.opacity = 1;
+};
+submitUpdateBtn.onclick = () => {
+  let title = document.querySelector(".title").value;
+  let body = document.querySelector(".body").value;
+  let input = { title, body };
+  const endPoint = `/blogs/${updateBtn.dataset.doc}`;
+  fetch(endPoint, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(input),
+  }).then((response) =>
+    response.json().then((data) => (window.location.href = data.redirect))
+  );
+};
