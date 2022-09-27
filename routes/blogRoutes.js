c1ac81d16b4fd7c052b1ed50a8b95a0052e2dfc1
Panagiotis <panfanartzis@gmail.com>
2022-09-27 21:51:00 +0300

    ~

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const blogRouter = require("../controllers/blogController");
+const blogController = require("../controllers/blogController");
 
-router.get("/", blogRouter.blog_index);
-router.get("/create", blogRouter.blog_create_get);
-router.get("/:id", blogRouter.blog_details); //an auto mpei pano apo to create, sto /create tha trexei auto to route
-router.post("/", blogRouter.blog_create_post);
-router.delete("/:id", blogRouter.blog_delete);
-router.put("/:id", blogRouter.blog_put);
+router.get("/", blogController.blog_index);
+router.get("/create", blogController.blog_create_get);
+router.get("/:id", blogController.blog_details); //an auto mpei pano apo to create, sto /create tha trexei auto to route
+router.post("/", blogController.blog_create_post);
+router.delete("/:id", blogController.blog_delete);
+router.put("/:id", blogController.blog_put);
 
 module.exports = router;
